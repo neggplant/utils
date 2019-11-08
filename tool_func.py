cb5d28bc3d5a5d49cbda394ed2c7558648da9e35
neggplant <1264021636@qq.com>
2019-11-08 14:20:18 +0800

    Update tool_func.py

--- tool_func.py
+++ tool_func.py
@@ -6,11 +6,11 @@ def timer(func):
         t0 = time.time()
         func(*args,**kwds)
         t1 = time.time()
-        print('wrapper耗时%0.3f'%(t1-t0,))
+        print('耗时{:.2f}'.format(t1-t0))
     return wrapper
 
 @timer
 def do_something(delay):
     print('函数do_something开始')
     time.sleep(delay)
-    print('函数do_something结束')
+    print("do_something", end="")
