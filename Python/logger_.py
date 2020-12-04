13d1be97b2c6c6be4c5c5393d1ee238ae03e1157
chenqian <cq4050@163.com>
2020-12-04 14:19:42 +0800

    增加日志按天切分，日期放在前

--- Python/logger_.py
+++ Python/logger_.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 from logging.handlers import TimedRotatingFileHandler
 
 
-a = True
+a = False
 b = True
 if a:
     # 不使用日志回滚
@@ -36,9 +36,10 @@ else:
     logger = logging.getLogger(__name__)
     logger.setLevel(level=logging.INFO)
     # 按文件大小备份，定义一个RotatingFileHandler，最多备份3个日志文件，每个日志文件最大1K，日志文件名后面加.1
-    rHandler = RotatingFileHandler("log.log", maxBytes=1 * 1024, backupCount=3)
+    # rHandler = RotatingFileHandler("log.log", maxBytes=1 * 1024, backupCount=3)
     # 按文件大小备份，定义一个RotatingFileHandler，最多备份3个日志文件，日志文件名后面加."%Y-%m-%d"
-    # rHandler = TimedRotatingFileHandler("log.log", when="D", backupCount=3)
+    rHandler = TimedRotatingFileHandler("a.log", when="S", backupCount=3)
+    rHandler.suffix = "-%Y-%m-%d_%H-%M-%S.log"
     formatter = logging.Formatter(
         fmt='%(asctime)s.%(msecs)05d %(levelname)s - %(filename)s - [line:%(lineno)d] - %(message)s',
         datefmt="%Y-%m-%d %H:%M:%S")
@@ -53,8 +54,14 @@ else:
     logger.addHandler(rHandler)
     logger.addHandler(console)
 
+def main():
+    logger.info("Start print log")
+    logger.debug("Do something")
+    logger.warning("Something maybe fail.")
+    logger.info("Finish")
 
-logger.info("Start print log")
-logger.debug("Do something")
-logger.warning("Something maybe fail.")
-logger.info("Finish")
+if __name__ == "__main__":
+    import time
+    for i in range(0, 5):
+        main()
+        time.sleep(2)
